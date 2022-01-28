768df87f81c94ae120bb2a1bd73ac17c524a15ad
ritikshri19 <ritikshri19@gmail.com>
2022-01-28 13:40:43 +0530

    Button integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PlugConnect from "@psychedelic/plug-connect";
 
 function App() {
+  const network = "";
+  const whitelist = "";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PlugConnect
+      dark
+      title="Connect To Plug"
+      // host={network}
+      // whitelist={whitelist}
+    />
   );
 }
 
