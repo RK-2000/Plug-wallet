4bd4a5638e1b39e417d54368efdf701cac76c678
ritikshri19 <ritikshri19@gmail.com>
2022-01-28 13:46:22 +0530

    Button integrated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 import PlugConnect from "@psychedelic/plug-connect";
 
 function App() {
+  async function getBalance() {
+    let balance = await window.ic.plug.requestBalance();
+    console.log(balance);
+  }
+
   const network = "";
   const whitelist = "";
   return (
@@ -11,6 +16,7 @@ function App() {
       title="Connect To Plug"
       // host={network}
       // whitelist={whitelist}
+      onConnectCallback={getBalance}
     />
   );
 }
