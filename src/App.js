b806497bbaf22518505de0c13b141dc0d012ad15
ritikshri19 <ritikshri19@gmail.com>
2022-01-29 17:03:55 +0530

    Button Component Added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import PlugConnect from "@psychedelic/plug-connect";
-import Balance from "./Components/Balance";
-import { StrictMode } from "react";
 import { Fragment } from "react";
+import Button from "./Components/Button";
 function App() {
-  // Returns Balance and Wallet Address
-  async function getBalance() {
-    // Gets Balance of all the tokens in wallet
-    let balance = await window.ic.plug.requestBalance();
-    for (let i = 0; i < balance.length; i++) {
-      // Finds the ICP Account and returns it balance
-      if (balance[i]["name"] == "ICP") {
-        console.log(" Balance for ICP : ", balance[i]["amount"]);
-        // Create an agent
-        await window.ic.plug.createAgent();
-        // Get agent Wallet ID
-        let id = await window.ic.plug.agent.getPrincipal();
-        console.log(id.toText());
-      }
-    }
-    balance.forEach((token) => {});
-  }
-
   return (
     <Fragment>
-      <PlugConnect
-        dark
-        title="Connect To Plug"
-        onConnectCallback={getBalance}
-      />
+      <Button />
     </Fragment>
   );
 }
---
+++ src/Components/Button.js
@@ -0,0 +1,51 @@
+import PlugConnect from "@psychedelic/plug-connect";
+import React from "react";
+
+class Button extends React.Component {
+  state = {
+    wallet: "Not Connected",
+    bal: 0,
+  };
+
+  getBalance = async () => {
+    // Gets Balance of all the tokens in wallet
+    let balance = await window.ic.plug.requestBalance();
+    for (let i = 0; i < balance.length; i++) {
+      // Finds the ICP Account and returns it balance
+      if (balance[i]["name"] == "ICP") {
+        this.setState({ bal: balance[i]["amount"] });
+        console.log(" Balance for ICP : ", balance[i]["amount"]);
+        // Create an agent
+        await window.ic.plug.createAgent();
+        // Get agent Wallet ID
+        let id = await window.ic.plug.agent.getPrincipal();
+        this.setState({ wallet: id.toText() });
+        console.log(id.toText());
+      }
+    }
+  };
+
+  render() {
+    const wallet = this.state.wallet;
+    let content;
+    if (wallet == "Not Connected") {
+      content = (
+        <PlugConnect
+          dark
+          title="Connect To Plug"
+          onConnectCallback={this.getBalance}
+        />
+      );
+    } else {
+      content = (
+        <div>
+          <p>Wallet = {this.state.wallet}</p>
+          <p>Balance = {this.state.bal}</p>
+        </div>
+      );
+    }
+    return <React.Fragment>{content}</React.Fragment>;
+  }
+}
+
+export default Button;
